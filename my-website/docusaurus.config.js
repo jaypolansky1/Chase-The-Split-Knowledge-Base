66e805d32944f7590e6fd37d942cf81ee8a8cc8c
jaypolansky1 <jaypolansky@gmail.com>
2026-07-20 00:23:23 -0400

    Update copyright text in docusaurus.config.js

--- my-website/docusaurus.config.js
+++ my-website/docusaurus.config.js
@@ -147,7 +147,7 @@ const config = {
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} Chase The Split Built with Docusaurus.`,
       },
       prism: {
         theme: prismThemes.github,
