ab86bf9686d06253acbb9f58bdf73064198326f3
jaypolansky1 <jaypolansky@gmail.com>
2026-07-20 23:30:34 -0400

    Update docusaurus.config.js

--- my-website/docusaurus.config.js
+++ my-website/docusaurus.config.js
@@ -126,17 +126,7 @@ const config = {
              
             ],
           },
-          {
-            title: 'More',
-            items: [
-              {
-                label: 'Blog',
-                to: '/blog',
-              },
-              {
-                label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
-              },
+         
             ],
           },
         ],
