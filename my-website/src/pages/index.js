43e034c2232af1ed18c527adea6f43fd4c70a7d6
jaypolansky1 <jaypolansky@gmail.com>
2026-07-21 23:31:55 -0400

    Change page title to 'Chase The Split Knowledge Base'

--- my-website/src/pages/index.js
+++ my-website/src/pages/index.js
@@ -32,7 +32,7 @@ export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
+      title={`Chase The Split Knowledge Base`}
       description="Description will go into a meta tag in <head />">
       <HomepageHeader />
       <main>
